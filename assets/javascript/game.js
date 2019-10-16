2c6786c0f29ba9d7744544b162dec106a804db9d
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-15 20:04:06 -0500

    2019-10-15: I pseudo coded my game.js file so I could have a plan of execution for my javascript logic.I

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//assign the target score to be a random number between 19-120
+    //the target score should reset and change each time the user wins or losses
+
+//assign each planet-img to a random number between 1-12
+    //the planet-img values should reset and change each time the user wins or losses
+
+//value of each planet-img appears when clicked in the 'your score' card of the html
+
+//log user wins in the 'wins' card of the html
+
+//log the user losses in the 'losses' card of the html
+
+//reset page when user wins or losses but does not get rid of the scoreboard
