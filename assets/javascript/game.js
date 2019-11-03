24705a4d81d01162527607988b0f68cd827a8ad1
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-11-02 20:23:16 -0500

    Added reset functions to my javascript page, trying to get the page to reset when the user wins or losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,19 @@
-//assign the target score to be a random number between 19-120.
-//the target score should reset and change each time the user wins or losses.
-
+//Variables
 let counter = 0;
+let usersScore = 0;
 let wins = 0;
 let losses = 0;
 
-$(document).ready(function() {
-    const targetNumber = (Math.floor(Math.random()*120) + 19);
-    $("#get-this-number").text(targetNumber);
-    let sum = 0;
-   
 
+//To create planet images
 const planet1 = {
     name: "mercury",
-    src: "./assets/images/mercury.jpg", 
+    src: "./assets/images/mercury.jpg",
 }
 
 const planet2 = {
     name: "venus",
-    src: "./assets/images/venus.jpg",      
+    src: "./assets/images/venus.jpg",
 }
 
 const planet3 = {
@@ -32,34 +27,65 @@ const planet4 = {
 }
 
 
+//To randomly generate a number between 20-120
+$(document).ready(function () {
+    const targetNumber = (Math.floor(Math.random() * 120) + 19);
+    $("#get-this-number").text(targetNumber);
+    let sum = 0;
 
-const planets = [planet1, planet2, planet3, planet4]
-for(let i=0; i < planets.length; i++) {
-    console.log (planets[i])
-    console.log (planets[i].src)
+    // to update planet values after each game
+    function updatePlanets() {
+        for (var i = 0; i < planetsArray.length; i++) {
+            var randomVal = Math.floor(Math.random() * 15 + 1);
+            $("#planetDiv" + i).attr('val', randomVal)
+        }
+    }
 
-    const newPlanet = $("<img>")
+    // to reset game
+    function resetGame() {
+        updatePlanets();
+        targetScoreSelector();
+        usersScore = 0;
+        $("#usersScore-text").text(usersScore);
+    }
 
-    newPlanet.attr("src", planets[i].src)
-    newPlanet.attr("id", planets[i].name)
-    newPlanet.addClass("img-thumbnail")
-    newPlanet.attr("planetValue", (Math.floor(Math.random()*12) + 1))
- 
-    $("#planetdiv").append(newPlanet)
 
-}
 
-$("#planetdiv").on("click", ".img-thumbnail", function() {
-    let planetValue = $(this).attr("planetValue");
+    //Functions
+    const planets = [planet1, planet2, planet3, planet4]
+    for (let i = 0; i < planets.length; i++) {
+        // console.log (planets[i])
+        // console.log (planets[i].src)
+        const newPlanet = $("<img>")
+        newPlanet.attr("src", planets[i].src)
+        newPlanet.attr("id", planets[i].name)
+        newPlanet.addClass("img-thumbnail")
+        newPlanet.attr("planetValue", (Math.floor(Math.random() * 12) + 1))
+        $("#planetdiv").append(newPlanet)
+    }
 
-    planetValue = parseInt(planetValue);
-    counter += planetValue;
+    $("#planetdiv").on("click", ".img-thumbnail", function () {
+        let planetValue = $(this).attr("planetValue");
+        planetValue = parseInt(planetValue);
+        counter += planetValue;
 
-    $("#usersScore").text(counter);
- 
-}); //closes planetdiv
+        $("#usersScore").text(counter);
+
+        if (counter === targetScore) {
+            alert("You win!");
+            wins++;
+            $("#userWins").text(userWins);
+            resetGame();
+
+        } else if (counter > targetScore) {
+            alert("Final User Score: " + counter + ". You busted! Try again!");
+            losses++;
+            $("#userLosses").text(userLosses);
+            resetGame();
+        }
+
+    }); //closes planetdiv
 
 
 
 }); // closes document.ready   
-
